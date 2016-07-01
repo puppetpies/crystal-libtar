0fdb5f23aca909ae7961309ad17e7e5634ae1cc8
Bri in The Sky <brianh6854@googlemail.com>
2016-07-01 23:11:47 +0100

    Untest much more TODO tommorow

--- src/crystal-libtar.cr
+++ src/crystal-libtar.cr
@@ -1,5 +1,75 @@
 require "./crystal-libtar/*"
 
-module Crystal::Libtar
-  # TODO Put your code here
+module Archiver
+  
+  class Tar
+    
+    property? t : Pointer(Pointer(LibTar::Tar))
+    
+    def initialize
+      @t = uninitialized Pointer(Pointer(LibTar::Tar))
+    end
+    
+    def set_tartype(type)
+      case type
+      when "openfunc"
+        r = LibTar::TartypeT.new.openfunc
+      when "closefunc"
+        r = LibTar::TartypeT.new.closefunc
+      when "readfunc"
+        r = LibTar::TartypeT.new.readfunc
+      when "writefunc"
+        r = LibTar::TartypeT.new.writefunc
+      else
+        r = LibTar::TartypeT.new.openfunc
+      end
+      return r
+    end
+    
+    def open(pathname : String, type, oflags : Int32, mode : Int32, options : Int32)
+      m = LibTar::TartypeT.new
+      t_type = pointerof(m) # set_tartype(type)
+      LibTar.tar_open(@t, pathname, t_type, oflags, mode, options)
+    end
+
+    def openfd(fd : Int32, pathname : String, type, oflags : Int32, mode : Int32, options : Int32)
+      m = LibTar::TartypeT.new
+      t_type = pointerof(m) # set_tartype(type)
+      LibTar.tar_openfd(@t, fd, pathname, t_type, oflags, mode, options)
+    end
+    
+    def append_file(realname : String, savefile : String)
+      LibTar.tar_append_file(@t, realname, savefile)
+    end
+
+    def append_regfile(realname : String)
+      LibTar.tar_append_regfile(@t, realname)
+    end
+    
+    def th_read
+      LibTar.th_read(@t)
+    end
+    
+    def th_write
+      LibTar.th_write(@t)
+    end
+    
+    def append_eof
+      LibTar.tar_append_eof(@t)
+    end
+    
+    def fd
+      LibTar.tar_fd(@t)
+    end
+    
+    def close
+      LibTar.tar_close(@t)
+    end
+    
+  end
+  
 end
+
+#t = Archiver::Tar.new
+#t.open("README.tar", "openfunc", 0, 644, 0)
+
